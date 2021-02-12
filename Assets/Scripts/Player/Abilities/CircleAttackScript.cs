b0d358e8190c7e51e5c2411247314c71f7101bb3
Adam Abdalla <aabdalla@berkeley.edu>
2021-02-12 18:38:40 -0500

    Finished Project 1 part 2.

---
+++ Assets/Scripts/Player/Abilities/CircleAttackScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleAttackScript : Ability
+{
+    public override void Use(Vector3 spawnPos) {
+        RaycastHit[] hits = Physics.SphereCastAll(spawnPos, m_Info.Range, transform.forward, 0);
+        foreach (RaycastHit hit in hits) {
+            if (hit.collider.CompareTag("Enemy")) {
+            hit.collider.GetComponent<EnemyController>().DecreaseHealth(m_Info.Power);
+            }
+        }
+        var emitterShape = cc_PS.shape;
+        emitterShape.length = m_Info.Range;
+        cc_PS.Play();
+    }
+}
